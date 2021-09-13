82560c4ccb7592e9900a6b2ec01bba0fd1e7cef8
Annmarie99 <62233264+Annmarie99@users.noreply.github.com>
2021-09-13 20:41:06 +0700

    first commit : vue project setup with bootstrap

---
+++ src/components/MainLayout.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+      <navbar v-if="!isHome" />
+      <slot/>
+  </div>
+</template>
+
+<script>
+import Navbar from './Layouts/Navbar.vue'
+
+export default {
+  components: { Navbar },
+    name : "MainLayout",
+    computed:{
+      isHome(){
+        return this.$route.path === "/"
+      }
+    }
+}
+</script>
+
+<style>
+
+.sidebar {
+  height: 100vh;
+  background: black;
+}
+
+</style>
---
+++ src/views/Note.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="row">
+      <div class="col-3 sidebar bg-black px-0"> <note-sidebar/></div>
+      <div class="col-9">
+        <p>Hello, note !</p>
+      </div>
+  </div>
+</template>
+
+<script>
+import NoteSidebar from '../components/Layouts/NoteSidebar.vue'
+export default {
+  components: { NoteSidebar },
+  name : "Note"
+}
+</script>
