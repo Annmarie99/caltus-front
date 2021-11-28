12213991d697d97758e3d1f1447d081e50852e2e
Annmarie99 <62233264+Annmarie99@users.noreply.github.com>
2021-11-28 19:06:00 +0700

    update/more

--- src/views/Cards/Learnmore.vue
+++ src/views/Cards/Learnmore.vue
@@ -1,213 +1,180 @@
-<template >
-
- <div class="flex p-0 m-0 rounded-tl-lg">
-<!--<div
-        class="
-          flex-1
-          space-y-5
-          bg-gradient-to-r
-          from-green-600
-          to-gray-700
-          p-3
-          rounded-tl-lg
-        "
-      >-->
-  <b-container1 class="m-10">
-
-  <b-row align-v="center"  >
-  
-    <b-col md="3">
-
-    <b-card
-    
-    img-src="https://scontent.fhdy3-1.fna.fbcdn.net/v/t1.15752-9/259010584_4522388111216081_7651173519138874606_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=ae9488&_nc_ohc=S1k0VUwR4lkAX_VbHIK&tn=9HuvVjk_7ElYvQ78&_nc_ht=scontent.fhdy3-1.fna&oh=4bed873268a57c2145aa40945105bb16&oe=61C81F29"
-    img-alt="Image"
-    img-top
-    title=""
-    tag="article"
-    style="max-width: 50rem;"
-    class="mb-2 "
-  >
-
-    <router-link to="/CardsApy">APY CALCULATOR</router-link>
-    </b-card>
-    </b-col>
-  
-    <b-col md="3" >
-
-    <b-card
-    img-src="https://scontent.fhdy3-1.fna.fbcdn.net/v/t1.15752-9/260322683_376251510962015_727481466743328698_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=ae9488&_nc_ohc=yRYjKAnFNkYAX8RJ81P&_nc_ht=scontent.fhdy3-1.fna&oh=5e6965813031f5ce74c4eec795651e05&oe=61C9EBA2"
-    img-alt="Image"
-    img-top
-    title=""
-    tag="article"
-    style="max-width: 50rem;"
-    class="mb-2 "
-  >
-    
-    <router-link to="/CardsPercent">PERCENTAGE CHANGE CALCULATOR</router-link>
-
-    </b-card>
-    </b-col>
-
-
-   <b-col md="3" >
-    <b-card
-    img-src="https://scontent.fhdy3-1.fna.fbcdn.net/v/t1.15752-9/259303666_4875961395794023_5131670985968585378_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=ae9488&_nc_ohc=gSZz2OiLJ_4AX-iikSo&_nc_ht=scontent.fhdy3-1.fna&oh=f8fddb6b480f7a92b66a855544680420&oe=61C86F06"
-    img-alt="Image"
-    img-top
-    title=""
-    tag="article"
-    style="max-width: 50rem;"
-    class="mb-2"
-  >
-
-   <router-link to="/CardsFee">FEE CALCULATOR</router-link>
-    </b-card>
-    </b-col> 
-
-
-
-    <b-col md="3" >
-    <b-card
-    img-src="https://i.pinimg.com/564x/35/bf/08/35bf0818ead3d5ffeab0d9c10390fb1f.jpg"
-    img-alt="Image"
-    img-top
-    title=""
-    tag="article"
-    style="max-width: 50rem;"
-    class="mb-2"
-  >
-  
-    <router-link to="/CardsAverage">AVERAGE DOWN CALCULATOR</router-link>
-
-    </b-card>
-    </b-col> 
-  </b-row>
-    <br>
-
-    
-
-<b-row align-v="center" >
-
-  <b-col md="3">
-    <b-card
-    img-src="https://scontent.fhdy3-1.fna.fbcdn.net/v/t1.15752-9/259076539_985162648702317_8817887497097431482_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=ae9488&_nc_ohc=3QlfT5vBhxwAX8YPeTt&_nc_ht=scontent.fhdy3-1.fna&oh=bcaab915922f3eea3dd35ded030bf73e&oe=61C7AA3D"
-    img-alt="Image"
-    img-top
-    title=""
-    tag="article"
-    style="max-width: 15rem;"
-    class="mb-2"
-  >
-    
-
-   <router-link to="/CardsLiqui">LIQUIDATION CALCULATOR</router-link>
-    </b-card>
-    </b-col>
-
-  <b-col md="3">
-    <b-card
-     img-src="https://scontent.fhdy3-1.fna.fbcdn.net/v/t1.15752-9/260869726_335600658014199_630453567850997379_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=ae9488&_nc_ohc=LGGfwYyh7nkAX9ZuJFX&tn=9HuvVjk_7ElYvQ78&_nc_ht=scontent.fhdy3-1.fna&oh=c378bd8050881b7e30304bad5f0c1872&oe=61C75A63"
-    img-alt="Image"
-    img-top
-    title=""
-    tag="article"
-    style="max-width: 15rem;"
-    class="mb-2"
-  >
-  
-
-   <router-link to="/CardsPNL">TRADE COMPOUND CALCULATOR</router-link>
-    </b-card>
-    </b-col>
-
-  <b-col md="3">
-    <b-card
-     img-src="https://scontent.fhdy3-1.fna.fbcdn.net/v/t1.15752-9/260770282_198435815713377_3326628479898582180_n.png?_nc_cat=111&ccb=1-5&_nc_sid=ae9488&_nc_ohc=iCe4b_i_MfkAX8jHwL2&_nc_oc=AQkJy1zQfaMR6TAEWacUq4NUgqtszDC5kYzwbBCzW2Kx8IYY8j2LBlmRgyWTnjAtUaM&tn=9HuvVjk_7ElYvQ78&_nc_ht=scontent.fhdy3-1.fna&oh=d17f1eed673f69417aedb4068f6f7932&oe=61C7D1AA"
-    img-alt="Image"
-    img-top
-    title=""
-    tag="article"
-    style="max-width: 15rem;"
-    class="mb-2"
-  >
-   
-   <router-link to="/CardsTrade">PNL CALCULATOR</router-link>
-    </b-card>
-    </b-col> 
-
-
-  <b-col md="3" >
-    <b-card
-     img-src="https://scontent.fhdy3-1.fna.fbcdn.net/v/t1.15752-9/260156402_3399530276940399_70434538516228695_n.png?_nc_cat=111&ccb=1-5&_nc_sid=ae9488&_nc_ohc=jZt_JgX63hIAX8dQJ_q&_nc_ht=scontent.fhdy3-1.fna&oh=eedd3ecab52c30a3c68019a84f744d9e&oe=61C7FFD9"
-    img-alt="Image"
-    img-top
-    title=""
-    tag="article"
-    style="max-width: 50rem;"
-    class="mb-2"
-  >
-    
-
-    <router-link to="/CardsLeverged">LEVERGED MOVEMENT CALCULATOR</router-link>
-    </b-card>
-    </b-col> 
-    </b-row>
-    <br>
-
-<b-pagination
-      v-model="currentPage"
-      :total-rows="rows"
-      :per-page="perPage"
-      first-text="First"
-      prev-text="Prev"
-      next-text="Next"
-      last-text="Last"
-    ></b-pagination>
-
-  </b-container1>
-
-</div>
- 
+<template>
+  <div class="container1 mt-20 ml-20">
+    <div class="row">
+      <div class="col text-white">
+        <b-col md="3">
+          <b-card
+            img-src="https://scontent.fhdy3-1.fna.fbcdn.net/v/t1.15752-9/259010584_4522388111216081_7651173519138874606_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=ae9488&_nc_ohc=S1k0VUwR4lkAX_VbHIK&tn=9HuvVjk_7ElYvQ78&_nc_ht=scontent.fhdy3-1.fna&oh=4bed873268a57c2145aa40945105bb16&oe=61C81F29"
+            img-alt="Image"
+            img-top
+            title=""
+            tag="article"
+            class="mb-2 morecard"
+          >
+            <router-link to="/CardsApy">APY CALCULATOR</router-link>
+          </b-card>
+        </b-col>
+      </div>
+      <div class="col text-white">
+        <b-col md="3">
+          <b-card
+            img-src="https://scontent.fhdy3-1.fna.fbcdn.net/v/t1.15752-9/260322683_376251510962015_727481466743328698_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=ae9488&_nc_ohc=yRYjKAnFNkYAX8RJ81P&_nc_ht=scontent.fhdy3-1.fna&oh=5e6965813031f5ce74c4eec795651e05&oe=61C9EBA2"
+            img-alt="Image"
+            img-top
+            title=""
+            tag="article"
+            class="mb-2 morecard"
+          >
+            <router-link to="/CardsPercent"
+              >PERCENTAGE CHANGE CALCULATOR</router-link
+            >
+          </b-card>
+        </b-col>
+      </div>
+      <div class="col text-white">
+        <b-col md="3">
+          <b-card
+            img-src="https://scontent.fhdy3-1.fna.fbcdn.net/v/t1.15752-9/259303666_4875961395794023_5131670985968585378_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=ae9488&_nc_ohc=gSZz2OiLJ_4AX-iikSo&_nc_ht=scontent.fhdy3-1.fna&oh=f8fddb6b480f7a92b66a855544680420&oe=61C86F06"
+            img-alt="Image"
+            img-top
+            title=""
+            tag="article"
+            class="mb-2 morecard"
+          >
+            <router-link to="/CardsFee">FEE CALCULATOR</router-link>
+          </b-card>
+        </b-col>
+      </div>
+      <div class="col text-white">
+        <b-col md="3">
+          <b-card
+            img-src="https://i.pinimg.com/564x/35/bf/08/35bf0818ead3d5ffeab0d9c10390fb1f.jpg"
+            img-alt="Image"
+            img-top
+            title=""
+            tag="article"
+            class="mb-2 morecard"
+          >
+            <router-link to="/CardsAverage"
+              >AVERAGE DOWN CALCULATOR</router-link
+            >
+          </b-card>
+        </b-col>
+      </div>
+    </div>
+    <div class="row mt-5">
+      <div class="col text-white">
+        <b-col md="3">
+          <b-card
+            img-src="https://scontent.fhdy3-1.fna.fbcdn.net/v/t1.15752-9/259076539_985162648702317_8817887497097431482_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=ae9488&_nc_ohc=3QlfT5vBhxwAX8YPeTt&_nc_ht=scontent.fhdy3-1.fna&oh=bcaab915922f3eea3dd35ded030bf73e&oe=61C7AA3D"
+            img-alt="Image"
+            img-top
+            title=""
+            tag="article"
+            class="mb-2 morecard"
+          >
+            <router-link to="/CardsLiqui">LIQUIDATION CALCULATOR</router-link>
+          </b-card>
+        </b-col>
+      </div>
+      <div class="col text-white">
+        <b-col md="3">
+          <b-card
+            img-src="https://scontent.fhdy3-1.fna.fbcdn.net/v/t1.15752-9/260869726_335600658014199_630453567850997379_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=ae9488&_nc_ohc=LGGfwYyh7nkAX9ZuJFX&tn=9HuvVjk_7ElYvQ78&_nc_ht=scontent.fhdy3-1.fna&oh=c378bd8050881b7e30304bad5f0c1872&oe=61C75A63"
+            img-alt="Image"
+            img-top
+            title=""
+            tag="article"
+            class="mb-2 morecard"
+          >
+            <router-link to="/CardsPNL">TRADE COMPOUND CALCULATOR</router-link>
+          </b-card>
+        </b-col>
+      </div>
+      <div class="col text-white">
+        <b-col md="3">
+          <b-card
+            img-src="https://scontent.fhdy3-1.fna.fbcdn.net/v/t1.15752-9/260770282_198435815713377_3326628479898582180_n.png?_nc_cat=111&ccb=1-5&_nc_sid=ae9488&_nc_ohc=iCe4b_i_MfkAX8jHwL2&_nc_oc=AQkJy1zQfaMR6TAEWacUq4NUgqtszDC5kYzwbBCzW2Kx8IYY8j2LBlmRgyWTnjAtUaM&tn=9HuvVjk_7ElYvQ78&_nc_ht=scontent.fhdy3-1.fna&oh=d17f1eed673f69417aedb4068f6f7932&oe=61C7D1AA"
+            img-alt="Image"
+            img-top
+            title=""
+            tag="article"
+            class="mb-2 morecard"
+          >
+            <router-link to="/CardsTrade">PNL CALCULATOR</router-link>
+          </b-card>
+        </b-col>
+      </div>
+      <div class="col text-white">
+        <b-col md="3">
+          <b-card
+            img-src="https://scontent.fhdy3-1.fna.fbcdn.net/v/t1.15752-9/260156402_3399530276940399_70434538516228695_n.png?_nc_cat=111&ccb=1-5&_nc_sid=ae9488&_nc_ohc=jZt_JgX63hIAX8dQJ_q&_nc_ht=scontent.fhdy3-1.fna&oh=eedd3ecab52c30a3c68019a84f744d9e&oe=61C7FFD9"
+            img-alt="Image"
+            img-top
+            title=""
+            tag="article"
+            class="mb-2 morecard"
+          >
+            <router-link to="/CardsLeverged"
+              >LEVERGED MOVEMENT CALCULATOR</router-link
+            >
+          </b-card>
+        </b-col>
+      </div>
+    </div>
+    <div class="containerr mt-3">
+      <div class="row">
+        <div class="ml-auto">
+          <b-pagination
+            class="center"
+            v-model="currentPage"
+            :total-rows="rows"
+            :per-page="perPage"
+            first-text="First"
+            prev-text="Prev"
+            next-text="Next"
+            last-text="Last"
+          ></b-pagination>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 //import CalculatorLayout from "../components/Layouts/CalculatorLayout.vue";
 export default {
-  data(){
+  data() {
     return {
       currentPage: 1,
       rows: 1,
       perPage: 3,
-      Image: {  width: 50, height: 75, class: 'm2' },
-    }
-
+      Image: { width: 50, height: 75, class: "m2" },
+    };
   },
-  async asyncData({$constants}){
-     const postsData = await $constants('posts').fetch()
-     return {
-        post: postsData,
-     };
-   },
+  async asyncData({ $constants }) {
+    const postsData = await $constants("posts").fetch();
+    return {
+      post: postsData,
+    };
+  },
 
   // //  methods:{
   // //  async fetchData(){
   // //    this.rows = this
-     
 
   //  }
 
-
   //  },
-   mounted(){
-      
-     console.log('THIS.POSTS', this.posts);
-
-    
-   }
+  mounted() {
+    console.log("THIS.POSTS", this.posts);
+  },
 };
 </script>
 
-
-
+<style>
+.morecard {
+  width: 15rem;
+  height: 15rem;
+}
+</style>
